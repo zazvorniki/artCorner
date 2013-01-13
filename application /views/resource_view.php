3ffe91be1df87ace030ba2fdf3439b28e0c85c72
zazvorniki <swampgliders@gmail.com>
2013-01-13 13:18:57 -0500

    admin add resource

---
+++ application /views/resource_view.php
@@ -0,0 +1,34 @@
+<!--  ========== blog post form ==========  -->
+
+<div class="container">
+	<div class="content">
+		<div class="writeBlog">
+			
+			<?=form_open('admin/insertResource', array('class' => 'postForm'));?>
+			<h3>Add a Resource!</h3>
+			
+			<p><label>Resource:</label> <input type="text" value="" name="resource" class="resInput" /></p>
+			
+						
+			<p><label>Title:</label> <input type="text" name="name" class="resInput"/></p>
+			
+			<p>
+				<label>Category</label>
+				<select id = "category">
+					<option value = "project">project</option>
+					<option value = "event">event</option>
+				</select>
+			</p>
+		
+			<p class="subPos"><input type="submit" class="submit" value="Post" /></p>
+			</form>
+		</div>
+	</div>
+</div>
+
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.2.js"></script>
+
+<script src="<?php echo base_url();?>inc/js/blogVal.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>inc/js/livevalidation_standalone.js" type="text/javascript"></script>
+		
---
+++ application /views/write_view.php
@@ -0,0 +1,37 @@
+<!--  ========== blog post form ==========  -->
+
+<div class="container">
+	<div class="content">
+		<div class="writeBlog">
+			
+			<?=form_open('admin/insertPost', array('class' => 'postForm'));?>
+			<h3>Write a new post!</h3>
+			
+			<p><label>Poster:</label> <input class="postInput" id="poster" type="text" value="Mrs. Sears" name="posted_by" readonly/></p>
+			
+			<p><label>Date:</label> <input class="postInput" id="date" type="text" class="date" name="date" value="<?print date("m/d/Y".' '.' '." g:i:s");?>" readonly/></p>
+			
+			<p><label>Title:</label> <input type="text" id="title" class="postInput" name="title"/></p>
+			
+			<p><label>Body:</label><textarea name="body" id="body" ></textarea></p>
+			
+			<p>
+				<label>Category</label>
+				<select id = "category">
+					<option value = "project">project</option>
+					<option value = "event">event</option>
+				</select>
+			</p>
+			
+			<p class="subPos"><input type="submit" class="submit" value="Post" /></p>
+			</form>
+		</div>
+	</div>
+</div>
+
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.2.js"></script>
+
+<script src="<?php echo base_url();?>inc/js/blogVal.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>inc/js/livevalidation_standalone.js" type="text/javascript"></script>
+		
