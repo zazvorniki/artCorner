431cdc9c76874ea36a1ba2d33ced13403f8cc694
zazvorniki <swampgliders@gmail.com>
2013-01-15 14:59:21 -0500

    backBtn styles

--- application/views/defaultBlog_view.php
+++ application/views/defaultBlog_view.php
@@ -1,19 +1,19 @@
 <!--  ==========	Body ==========  -->
 <div class="binder">
-<div class="container">
-	<div class="content">
-		<div class="blogContainer">
-			<ul class="item">
-			<?php foreach ($query->result() as $row): ?> 
-				<li>
-					<h3 class="bTitle"><?=$row->title?></h3>
-					<p class="bDate"><?=$row->date?></p>
-					<p class="bPosted">Posted by: <?=$row->posted_by?></p>
-					<p class="bBody"><?=$row->body?></p>
-					<p class="readMore"><?=anchor('blog/comments/'.$row->id, ' More->', array('class' => 'readMore'));?></p>
-					<hr>
-				</li>
-			<?php endforeach;?>
-			</ul>
-		</div>	
+	<div class="container">
+		<div class="content">
+			<div class="blogContainer">
+				<ul class="item">
+				<?php foreach ($query->result() as $row): ?> 
+					<li>
+						<h3 class="bTitle"><?=$row->title?></h3>
+						<p class="bDate"><?=$row->date?></p>
+						<p class="bPosted">Posted by: <?=$row->posted_by?></p>
+						<p class="bBody"><?=$row->body?></p>
+						<p class="readMore"><?=anchor('blog/comments/'.$row->id, ' More->', array('class' => 'readMore'));?></p>
+						<hr>
+					</li>
+				<?php endforeach;?>
+				</ul>
+			</div>	
 		</div>
--- application/views/innerBlog_view.php
+++ application/views/innerBlog_view.php
@@ -1,15 +1,16 @@
 <!--  ==========	Body ==========  -->
-
-<div class="container">
-	<div class="content">
-	<a class="backBtn" onclick="history.go(-1);"> ≤- back</a>
-		
-		<div class="innerBlog">
-			<?php foreach ($query->result() as $row): ?> 
-					<h3 class="bTitle"><?=$row->title?></h3>
-					<p class="bDate"><?=$row->date?></p>
-					<p class="bPosted">Posted by: <?=$row->posted_by?></p>
-					<p class="bBody"><?=$row->body?>
-					<hr>
-			<?php endforeach;?>
-		</div>	
+<div class="binder">
+	<div class="container">
+		<div class="content">
+		<a class="backBtn" onclick="history.go(-1);"> ≤- back</a>
+			
+			<div class="innerBlog">
+				<?php foreach ($query->result() as $row): ?> 
+						<h3 class="bTitle"><?=$row->title?></h3>
+						<p class="bDate"><?=$row->date?></p>
+						<p class="bPosted">Posted by: <?=$row->posted_by?></p>
+						<p class="bBody"><?=$row->body?>
+						<hr>
+				<?php endforeach;?>
+			</div>
+		</div>		
