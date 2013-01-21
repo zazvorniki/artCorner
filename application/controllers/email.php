17384acffc8bbdd02e23a1e88b12567f0c03d9e9
zazvorniki <swampgliders@gmail.com>
2013-01-21 12:23:58 -0500

    added contact form security

--- application/controllers/email.php
+++ application/controllers/email.php
@@ -11,40 +11,52 @@ class Email extends CI_Controller{
 	
 	function index()
 	{	
-		//this sets the configurations for the email. I am using a gmail account to send this email instead of the default because of how it shows up in the inbox.
-		$config = Array(
-			"protocol" => "smtp",
-			"smtp_host" => "ssl://smtp.googlemail.com",
-			"smtp_port" => "465",
-			"smtp_user" => "swampgliders@gmail.com",
-			"smtp_pass" => "tanfnntt"
-		);
-		
-		//this loads the email library. I would set this up in the autoload class except I am only sending one email so it does not make sense to load the class in every controller
-		$this->load->library('email', $config);
-		
-		//this makes sure that the html tags that are processed correctly in the email and style the email instead of printing the tags out.
-		$this->email->initialize(array('mailtype' => 'html')); 
-		
-		//from the research I've done, no one really knows why you have to put this, but if you don't then it throws an error
-		$this->email->set_newline("\r\n");
-		
-		//this sets the from field, the to field and he cc field. These will change once the site is in full production. At the moment the email is sending to my own personal email account and the cc is commented out
-		$this->email->from(strip_tags($this->input->post('email')), strip_tags($this->input->post('name')));
-		$this->email->to('swampgliders@gmail.com'); 
-//		$this->email->cc('swampgliders@gmail.com');
-		$this->email->reply_to(strip_tags($this->input->post('email')), strip_tags($this->input->post('name'))); 
-		
-		//the is the email subject and message that will be sent 
-		$this->email->subject('plantationkeyartcorner.com contact form');
-		$this->email->message($this->input->post('message'));	
-		
-		//this says if the send function does not work than it will throw an error, if it does work than it will send you to sent mail function
-		if (!$this->email->send())
+	
+	}
+	
+	function emailForm()
+	{
+		//this makes sure that this function can't be directly accessed through the url without the data-key in the form
+		if($this->input->post('data-key') == 'contactF')
 		{
-			show_error($this->email->print_debugger());
+			//this sets the configurations for the email. I am using a gmail account to send this email instead of the default because of how it shows up in the inbox.
+			$config = Array(
+				"protocol" => "smtp",
+				"smtp_host" => "ssl://smtp.googlemail.com",
+				"smtp_port" => "465",
+				"smtp_user" => "swampgliders@gmail.com",
+				"smtp_pass" => "tanfnntt"
+			);
+		
+			//this loads the email library. I would set this up in the autoload class except I am only sending one email so it does not make sense to load the class in every controller
+			$this->load->library('email', $config);
+			
+			//this makes sure that the html tags that are processed correctly in the email and style the email instead of printing the tags out.
+			$this->email->initialize(array('mailtype' => 'html')); 
+			
+			//from the research I've done, no one really knows why you have to put this, but if you don't then it throws an error
+			$this->email->set_newline("\r\n");
+			
+			//this sets the from field, the to field and he cc field. These will change once the site is in full production. At the moment the email is sending to my own personal email account and the cc is commented out
+			$this->email->from(strip_tags($this->input->post('email')), strip_tags($this->input->post('name')));
+			$this->email->to('swampgliders@gmail.com'); 
+			//		$this->email->cc('swampgliders@gmail.com');
+			$this->email->reply_to(strip_tags($this->input->post('email')), strip_tags($this->input->post('name'))); 
+			
+			//the is the email subject and message that will be sent 
+			$this->email->subject('plantationkeyartcorner.com contact form');
+			$this->email->message($this->input->post('message'));	
+			
+			//this says if the send function does not work than it will throw an error, if it does work than it will send you to sent mail function
+			if (!$this->email->send())
+			{
+				show_error($this->email->print_debugger());
+			}else{
+				redirect('email/sentMail');
+			}
+			
 		}else{
-			redirect('email/sentMail');
+			redirect('error/');
 		}
 	}
 
--- application/views/contact_view.php
+++ application/views/contact_view.php
@@ -2,9 +2,12 @@
 <div class="binder">
 	<div class="container">
 		<div class="content">
+			<p class="emailJava">Were sorry, but you need javascript to use our contact form. If you believe you are seeing this message in error please contact the creator of this site catlyndesigns@gmail.com </p>
+			<p class="emailJava">If you still need to contact the art teacher please email marcia.sears@keysschools.com</p>
 			<div class="contactForm">			
 				<h3>Contact Us!</h3>
-				<?=form_open('email/', array('class' => 'con'));?>
+				<?=form_open('email/emailForm/', array('class' => 'con'));?>
+					<?=form_hidden('data-key', 'contactF')?>
 					<p><label>Name:</label> <input class="contact" type="text" value="" name="name" id="name" autocomplete="off"/></p>
 					
 					<p><label>Email:</label> <input type="text" class="contact" name="email" id="email" autocomplete="off"/></p>
