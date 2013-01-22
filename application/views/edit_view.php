946baaf3d1f1c577f6cca9752a63ba15113c13b3
zazvorniki <swampgliders@gmail.com>
2013-01-21 19:00:50 -0500

    side information bar on adding blog and editing blog

--- application/views/edit_view.php
+++ application/views/edit_view.php
@@ -17,14 +17,34 @@
 					<div class="textPos">
 					<p><textarea name="body" id="body" > <?=$row->body?></textarea></p>
 					</div>
-					
+					*note: to create a new paragraph press the return twice
 					<p class="subPos"><input type="submit" class="submit" value="Save" autocomplete="off"/></p>
 					</form>
 				<?php endforeach;?>
 			</div>
-		</div>
-	</div>
-</div>	
+			
+			<div class="sideIcons">
+				<ul>
+					<li><img src="<?php echo base_url();?>inc/img/tinymce/bold.png" alt="" /><span> - Bold your text</span></li>
+					<li><img src="<?php echo base_url();?>inc/img/tinymce/italic.png" alt="" /><span> - Italicize your font</span></li>
+					<li><img src="<?php echo base_url();?>inc/img/tinymce/underline.png" alt="" /><span> - Underline your font</span></li>
+					<li><img src="<?php echo base_url();?>inc/img/tinymce/linethrough.png" alt="" /><span> - Put a line through your font</span></li>
+					<li><img src="<?php echo base_url();?>inc/img/tinymce/leftalign.png" alt="" /><span> - Left align</span></li>
+					<li><img src="<?php echo base_url();?>inc/img/tinymce/centered.png" alt="" /><span> - Center align</span></li>
+					<li><img src="<?php echo base_url();?>inc/img/tinymce/justified.png" alt="" /><span> - Justified</span></li>
+					<li><img src="<?php echo base_url();?>inc/img/tinymce/clean.png" alt="" /><span> - Clean up the code</span></li>
+					<li><img src="<?php echo base_url();?>inc/img/tinymce/color.png" alt="" /><span> - Add color to your font</span></li>
+					<li><img src="<?php echo base_url();?>inc/img/tinymce/spelling.png" alt="" /><span> - Check spelling</span></li>
+					<li><img src="<?php echo base_url();?>inc/img/tinymce/undo.png" alt="" /><span> - Undo</span></li>
+					<li><img src="<?php echo base_url();?>inc/img/tinymce/redo.png" alt="" /><span> - Redo</span></li>
+					<li><img src="<?php echo base_url();?>inc/img/tinymce/link.png" alt="" /><span> - Create a link</span></li>
+					<li><img src="<?php echo base_url();?>inc/img/tinymce/unlink.png" alt="" /><span> - Remove link</span></li>
+					<li><img src="<?php echo base_url();?>inc/img/tinymce/picture.png" alt="" /><span> - Insert a picture</span></li>
+				</ul>
+			</div>
+			
+			<div class="clear"></div>
+	
 <script src="<?php echo base_url();?>inc/js/blogVal.js" type="text/javascript"></script>
 
 		
