c2b6583fb2cf8e9a937703b0a8a7b46034ac6878
zazvorniki <swampgliders@gmail.com>
2013-01-25 17:48:46 -0500

    commenting the blog controller

--- application/controllers/blog.php
+++ application/controllers/blog.php
@@ -15,41 +15,33 @@ class Blog extends CI_Controller{
 	{
 		//this says that the session user is the current user
 		$user['currentUser']=$this->session->userdata('currentUser');
-
 		//this if statement says if there are user sessions then the user will be given the admin controls, but if they do not have the right sessions than they are given the views without the controls.
 		if (empty($user['currentUser'])) {
-			//This loads the default views. The header, body and footer
+			//This grabs the information from the model and then pushes the data into the default body view
 			$this->load->view('defaultHeader_view');
 			$data = array();
 			$query = $this->blog_model->loadAll();
-			
 			$data['query'] = $query;
 			$this->load->view('defaultBlog_view', $data);
-
-			
+			//This takes the information from the model and then pushes it into the default side bar
 			$d = array();
 			$q = $this->blog_model->loadResource();	
-			
 			$d['query'] = $q;
 			$this->load->view('defaultSidebar_view', $d);
-			
 			$this->load->view('footer_view');
 		}else {
 			//this takes the currentUser and then passes it to a function inside the user model
 			$user = $this->users_model->getUser($user['currentUser']->id);
-			//this loads the blog posts, the side bar and the footer
+			//This takes the model and pushes the information into the admin blog view
 			$data = array();
 			$query = $this->blog_model->loadAll();
-			
 			$data['query'] = $query;
 			$this->load->view('adminBlog_view', $data);
-			
+			//This takes the information from the model and pushes it into the admins side bar
 			$d = array();
 			$q = $this->blog_model->loadResource();	
-			
 			$d['query'] = $q;
 			$this->load->view('adminSidebar_view', $d);
-						
 			$this->load->view('footer_view');
 		} 
 	}
@@ -63,40 +55,32 @@ class Blog extends CI_Controller{
 		if (empty($user['currentUser'])) {
 			//this loads the default projects, then the body and the footer 
 			$this->load->view('defaultHeader_view');
-			
-			
+			//This takes the information for the projects and pushes it into the project default blog view
 			$data = array();
 			$query = $this->blog_model->loadProjects();
-			
 			$data['query'] = $query;
 			$this->load->view('defaultBlog_view', $data);
-			
-			
+			//This takes the information for the projects and pushes it into the project default sidebar		
 			$d = array();
 			$q = $this->blog_model->loadReProject();	
-			
 			$d['query'] = $q;
 			$this->load->view('defaultSidebar_view', $d);
-						
 			$this->load->view('footer_view');
 						
 		}else {
 			//this takes the currentUser and then passes it to a function inside the user model
 			$user = $this->users_model->getUser($user['currentUser']->id);
-			//this loads the project blogs and the sidebar and footer that goes along with it
+			//This takes the information for the projects and pushes it into the project admin blog view
 			$data = array();
 			$query = $this->blog_model->loadProjects();
-			
 			$data['query'] = $query;
 			$this->load->view('adminBlog_view', $data);
 			
-			
+			//This takes the information for the projects and pushes it into the project admin sidebar			
 			$d = array();
 			$q = $this->blog_model->loadReProject();	
-			
 			$d['query'] = $q;
 			$this->load->view('adminSidebar_view', $d);
-						
 			$this->load->view('footer_view');
 		}
 	}
@@ -107,41 +91,32 @@ class Blog extends CI_Controller{
 		$user['currentUser']=$this->session->userdata('currentUser');
 		//this if statement says if there are user sessions then the user will be given the admin controls, but if they do not have the right sessions than they are given the views without the controls.
 		if (empty($user['currentUser'])) {
-			//this loads the default events, then the body and the footer 
+			//This takes the information from the model and pushes it into the event default blog view
 			$this->load->view('defaultHeader_view');
 			$data = array();
 			$query = $this->blog_model->loadEvents();
-			
 			$data['query'] = $query;
 			$this->load->view('defaultBlog_view', $data);
-			
-			
+			//This takes the information from the model and pushes it into the event default sidebar
 			$d = array();
 			$q = $this->blog_model->loadReEvent();	
-			
 			$d['query'] = $q;
 			$this->load->view('defaultSidebar_view', $d);
-						
 			$this->load->view('footer_view');
-			
 		}else{
 			//this takes the currentUser and then passes it to a function inside the user model
 			$user = $this->users_model->getUser($user['currentUser']->id);
-			//this loads the default events, then the body and the footer 
-			
+			//This takes the information from the model and pushes it into the event admin blog view
 			$data = array();
 			$query = $this->blog_model->loadEvents();
-			
 			$data['query'] = $query;
 			$this->load->view('adminBlog_view', $data);
 			
-			
+			//This takes the information from the model and pushes it into the event sidebar admin view
 			$d = array();
 			$q = $this->blog_model->loadReEvent();	
-			
 			$d['query'] = $q;
 			$this->load->view('adminSidebar_view', $d);
-						
 			$this->load->view('footer_view');
 		}		
 	}
@@ -152,26 +127,22 @@ class Blog extends CI_Controller{
 		$user['currentUser']=$this->session->userdata('currentUser');
 		//this if statement says if there are user sessions then the user will be given the admin controls, but if they do not have the right sessions than they are given the views without the controls.
 		if (empty($user['currentUser'])) {
-			//this loads the default events, then the body and the footer 
-			$this->load->view('defaultHeader_view');
 
+			//This takes the information from the model and pushes it into the default vocab view
+			$this->load->view('defaultHeader_view');
 			$data = array();
 			$query = $this->blog_model->loadVocab();
-			
 			$data['query'] = $query;
 			$this->load->view('defaultVocab_view', $data);
-			
 			$this->load->view('footer_view');
 		}else{
 			//this takes the currentUser and then passes it to a function inside the user model
 			$user = $this->users_model->getUser($user['currentUser']->id);
-			//this loads the default events, then the body and the footer
+			//This takes the information from the model and pushes it into the admin vocab view
 			$data = array();
 			$query = $this->blog_model->loadVocab();
-			
 			$data['query'] = $query;
 			$this->load->view('adminVocab_view', $data);
-			 
 			$this->load->view('footer_view');		
 		}
 	}			
@@ -182,39 +153,28 @@ class Blog extends CI_Controller{
 		$user['currentUser']=$this->session->userdata('currentUser');
 		//this if statement says if there are user sessions then the user will be given the admin controls, but if they do not have the right sessions than they are given the views without the controls.
 		if (empty($user['currentUser'])) {
-			//this loads the comments
+			//This takes the information from the model and pushes it into the default one blog post view
 			$this->load->view('defaultHeader_view');
-			
-			
-			$data = array();
 			$query = $this->blog_model->loadOneEntry();
-			
 			$data['query'] = $query;
 			$this->load->view('innerBlog_view', $data);
-			
-			
+			//This takes the information from the model and pushes it into the default comment view
 			$d = array();
 			$q = $this->blog_model->loadComments();	
-			
 			$d['query'] = $q;
 			$this->load->view('comment_view', $d);
-			
-			
 			$this->load->view('footer_view');
 		}else {
 			//this takes the currentUser and then passes it to a function inside the user model
 			$user = $this->users_model->getUser($user['currentUser']->id);
-			//this loads the comments
-			
+			//This takes the information from the model and pushes it into the admin one blog post view
 			$data = array();
 			$query = $this->blog_model->loadOneEntry();
-			
 			$data['query'] = $query;
 			$this->load->view('adminInner_view', $data);
-			
+			//This takes the information from the model and pushes it into the admin comment view
 			$d = array();
 			$q = $this->blog_model->loadComments();	
-			
 			$d['query'] = $q;
 			$this->load->view('comment_view', $d);
 			$this->load->view('footer_view');
@@ -239,7 +199,6 @@ class Blog extends CI_Controller{
 	{
 		//this says that the session user is the current user
 		$user['currentUser']=$this->session->userdata('currentUser');
-		
 		//this if statement says if there are user sessions then the user will be given the admin controls, but if they do not have the right sessions than they are given the views without the controls.
 		if (empty($user['currentUser'])) {
 			//This loads the default views. The header, body and footer
