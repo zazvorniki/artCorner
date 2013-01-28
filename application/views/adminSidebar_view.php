fbcec901ae69492a40655ecd40cd3e36afc9fc48
zazvorniki <swampgliders@gmail.com>
2013-01-27 23:32:29 -0500

    delete white space in css and consadate some styles

--- application/views/adminSidebar_view.php
+++ application/views/adminSidebar_view.php
@@ -1,10 +1,10 @@
 <!--  ==========	Admin Sidebar ==========  -->
 <div class="ref">
 	<h3>Resources</h3>
-	<?foreach($query as $row) :?>	
-		<ul>
+	<ul>
+		<?foreach($query as $row) :?>	
 			<li><span class="deleteCon"><?=anchor('admin/deleteLink/'.$row->id, 'delete', array('class' => 'delete'));?></span><a href="<?=$row->resource?>"><?=$row->name?></a></li>
-		</ul>
-	<?endforeach;?>
+		<?endforeach;?>
+	</ul>
 </div><!--  end ref  -->
 <div class="clear"></div>
