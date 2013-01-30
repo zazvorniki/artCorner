2c6fb47d5ffabaa2428fb806839dd6503dae9909
zazvorniki <swampgliders@gmail.com>
2013-01-30 17:35:21 -0500

    onFocusOut validation on tinymce

--- application/views/defaultLetter_view.php
+++ application/views/defaultLetter_view.php
@@ -3,11 +3,14 @@
 	<div class="container">
 		<div class="content">
 			<div class="vocabPage">
-				<p class="bold"><?=anchor('blog/vocab/', 'Vocabulary');?> <?if($this->uri->segment(3)){echo ' - '.$this->uri->segment(3);};?></p>
+				<p class="bold"><?=anchor('blog/vocab/', 'Vocabulary');?> <?if($this->uri->segment(3)){echo ' - '.$this->uri->segment(3);}else{echo ' - All';};?></p>
 					<ul id="apl">
-						<li><?=anchor('blog/vocab/', 'View All');?></li>
+						<li><?=anchor('blog/vocab/', 'All');?></li>		
 						<?foreach($letter as $row) :?>
 							<li><?=anchor('blog/vocab/'.$row->letter, $row->letter, array('class' => 'letters'));?></li>
 						<? endforeach;?>
 					</ul>
 				<ul>
+				
+				
+				
